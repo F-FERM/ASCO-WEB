5814c4f1f5292047f37d49897a17ced527afcb2a
Anees <aneespengad4447@gmail.com>
2026-04-14 10:23:52 +0530

    feat(home): add services section
    - integrate ServicesSection component into the home page
    - provide users with information about available services

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,14 @@
 import HeroSection from "@/components/HeroSection";
 import HomeIntroSection from "@/components/HomeIntroSection";
+import ServicesSection from "@/components/ServicesSection";
 import React from "react";
 
 const Home = () => {
   return (
     <main>
       <HeroSection />
-      <HomeIntroSection/>
+      <HomeIntroSection />
+      <ServicesSection />
     </main>
   );
 };
