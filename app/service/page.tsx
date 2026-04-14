2f0fef7360cfe81a2f4ee3a12ef271fa674a5d51
Anees <aneespengad4447@gmail.com>
2026-04-14 11:41:00 +0530

    feat(service): add service intro section
    - introduce a new section for service introduction
    - include an image for the service intro section

--- app/service/page.tsx
+++ app/service/page.tsx
@@ -1,10 +1,12 @@
 import ServiceHeroSection from "@/components/service/ServiceHeroSection";
+import ServiceIntroSection from "@/components/service/ServiceIntroSection";
 import React from "react";
 
 const Service = () => {
   return (
     <main>
       <ServiceHeroSection />
+      <ServiceIntroSection />
     </main>
   );
 };
