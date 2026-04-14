d5ff0ffc1ad5fbb7ff239e521bf72ded962d5f6c
Anees <aneespengad4447@gmail.com>
2026-04-14 11:09:24 +0530

    feat(app): add service page
    - introduce a new service page component
    - integrate the service page into the main application layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import Home from "./(home)/page";
 import About from "./about/page";
+import Service from "./service/page";
 
 export default function Page() {
   return (
     <main>
       <Home />
-      <About/>
+      <About />
+      <Service />
     </main>
   );
 }
