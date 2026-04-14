3f3d586542351c128531b1e79b86d4c6c73a5c4d
Anees <aneespengad4447@gmail.com>
2026-04-14 12:35:20 +0530

    feat(page): remove unused components from home page
    - remove About, Service, Clients, Careers, and Contact components
    - simplify home page structure to only render the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,10 @@
 import Image from "next/image";
 import Home from "./(home)/page";
-import About from "./about/page";
-import Service from "./service/page";
-import Clients from "./clients/page";
-import Careers from "./careers/page";
-import Contact from "./contact/page";
 
 export default function Page() {
   return (
     <main>
       <Home />
-      <About />
-      <Service />
-      <Clients />
-      <Careers />
-      <Contact/>
     </main>
   );
 }
