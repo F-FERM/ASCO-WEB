55e865d7c844ca516b84a837afb12c3b23b3e3c2
Anees <aneespengad4447@gmail.com>
2026-04-14 11:42:24 +0530

    feat(clients): add clients page
    - create a new page component for clients
    - integrate the clients page into the main app layout

---
+++ app/clients/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Clients = () => {
+  return <main></main>;
+};
+
+export default Clients;
--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Home from "./(home)/page";
 import About from "./about/page";
 import Service from "./service/page";
+import Clients from "./clients/page";
 
 export default function Page() {
   return (
@@ -9,6 +10,7 @@ export default function Page() {
       <Home />
       <About />
       <Service />
+      <Clients />
     </main>
   );
 }
