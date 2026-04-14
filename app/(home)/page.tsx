c94f6b3413dd39f50d61ee60b78c87964dfd8fc5
Anees <aneespengad4447@gmail.com>
2026-04-14 09:46:12 +0530

    feat(home): implement home page layout
    - add hero section component
    - add home intro section component
    - structure home page with main element

---
+++ app/(home)/page.tsx
@@ -0,0 +1,14 @@
+import HeroSection from "@/components/HeroSection";
+import HomeIntroSection from "@/components/HomeIntroSection";
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <HeroSection />
+      <HomeIntroSection/>
+    </main>
+  );
+};
+
+export default Home;
