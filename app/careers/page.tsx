61261d5fbdef9335eadcaccfc972f48591af268a
Anees <aneespengad4447@gmail.com>
2026-04-14 11:54:38 +0530

    feat(careers): implement careers page
    - add new page component for careers section
    - include hero and content sections for career information

---
+++ app/careers/page.tsx
@@ -0,0 +1,14 @@
+import CareerContentSection from "@/components/careers/CareerContentSection";
+import CareerHeroSection from "@/components/careers/CareerHeroSection";
+import React from "react";
+
+const Careers = () => {
+  return (
+    <main>
+      <CareerHeroSection />
+      <CareerContentSection />
+    </main>
+  );
+};
+
+export default Careers;
