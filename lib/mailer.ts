dba992a8c05fcf15749974831d6c7a86098eff06
Anees <aneespengad4447@gmail.com>
2026-04-16 17:42:52 +0530

    feat(mailer): update smtp configuration
    - change service to host and port for gmail
    - set secure to false as recommended by nodemailer for gmail
    - remove pool, maxConnections, and maxMessages as they are no longer needed

--- lib/mailer.ts
+++ lib/mailer.ts
@@ -1,12 +1,11 @@
 import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
-  pool: true,
-  maxConnections: 5,
-  maxMessages: 100,
 });
