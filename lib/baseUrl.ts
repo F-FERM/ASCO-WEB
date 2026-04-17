30b2f95e8bb7832cebd48e902662e1fe094e13ec
Anees <aneespengad4447@gmail.com>
2026-04-17 16:08:17 +0530

    feat(api): add baseUrl configuration
    - introduce a new file `lib/baseUrl.ts` to manage API base URLs
    - export `baseURL` constant for consistent API endpoint referencing

---
+++ lib/baseUrl.ts
@@ -0,0 +1,3 @@
+export const baseURL = "http://localhost:5002/api"; 
+// export const baseURL = "https://f-ferm-api-admin.onrender.com/api"; 
+// export const baseURL = "https://api.f-ferm.com/api"; 
