00ceb33dcba92265925fdf38233e7081635de6fa
Anees <aneespengad4447@gmail.com>
2026-04-17 17:12:42 +0530

    feat(api): update base URL for admin service
    - change base URL to "https://admin.ascoqatar.com/api"
    - this change is to point to the new production environment for the admin API

--- lib/baseUrl.ts
+++ lib/baseUrl.ts
@@ -1,3 +1,2 @@
-export const baseURL = "http://localhost:5002/api"; 
-// export const baseURL = "https://f-ferm-api-admin.onrender.com/api"; 
-// export const baseURL = "https://api.f-ferm.com/api"; 
+// export const baseURL = "http://localhost:5002/api";
+export const baseURL = "https://admin.ascoqatar.com/api";
